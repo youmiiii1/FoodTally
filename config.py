d98f60e67a1fb7b4cdcfebf8f5ef299abb6645c5
youmee1 <leadgenmichaelnagronyy19@gmail.com>
2025-11-15 13:04:22 +0000

    Ready to deploy

--- config.py
+++ config.py
@@ -1,13 +1,15 @@
-# TG 'FoodTelly1.0' API
-TOKEN_BOT = "8395344387:AAFbRW1vYzrJ8cBP8aelI1BLg15CEGYJ0H4"
+import os
 
-#Make.com 'FoodTellyMakeComAI_bot'
-WEB_HOOK_URL = "https://hook.eu2.make.com/sn34lnlfw35lqc4u46pniwyaeyctc7c9"
+# TG 'FoodTelly1.0' API
+TOKEN_BOT = os.getenv("TOKEN_BOT")
+
+# Make.com webhook
+WEB_HOOK_URL = os.getenv("WEB_HOOK_URL")
 
 # database_config
-host = 'localhost'
-db_name = 'food_tally_db'
-db_password = '123456789'
-db_user = 'postgres'
-port = 5432
+host = os.getenv("DB_HOST", "localhost")
+db_name = os.getenv("DB_NAME", "food_tally_db")
+db_password = os.getenv("DB_PASSWORD", "")
+db_user = os.getenv("DB_USER", "postgres")
+port = int(os.getenv("DB_PORT", 5432))
 
