d09fe782a3c65b76e74c5db037030999204792d9
youmiiii1 <leadgenmichaelnagronyy19@gmail.com>
2025-11-23 16:16:19 +0000

    Clear config

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 # TG 'FoodTelly1.0' API
-TOKEN_BOT = "8395344387:AAFbRW1vYzrJ8cBP8aelI1BLg15CEGYJ0H4"
+TOKEN_BOT = ""
 
 #Make.com 'FoodTellyMakeComAI_bot'
-WEB_HOOK_URL = "https://hook.eu2.make.com/sn34lnlfw35lqc4u46pniwyaeyctc7c9"
+WEB_HOOK_URL = ""
 
 # database_config
-host = 'localhost'
-db_name = 'food_tally_db'
-db_password = '123456789'
-db_user = 'postgres'
+host = ''
+db_name = ''
+db_password = ''
+db_user = ''
 port = 5432
 
